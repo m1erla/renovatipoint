ce0285f3d5f3c177195c7dd81561700c73fd348f
Furkan Karakus <furkankarakus01@hotmail.com>
2023-08-29 12:40:34 +0300

    dataAccess ready to connect to data base

---
+++ backend/src/main/java/com/werkspot/dataAccess/abstracts/ConsumerRepository.java
@@ -0,0 +1,12 @@
+package com.werkspot.dataAccess.abstracts;
+
+import com.werkspot.entities.concretes.Consumer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+
+public interface ConsumerRepository extends JpaRepository<Consumer, Integer> {
+    boolean existsConsumersByEmail(String email);
+
+    boolean existsConsumerByPhoneNumber(String phoneNumber);
+}
