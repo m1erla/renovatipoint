fa91d1cebb69584bc032537be4625d65cdee7716
Furkan Karakus <furkankarakus01@hotmail.com>
2023-08-29 12:09:12 +0300

    core.utilities model mapper classes and @Bean annotation in application class

--- backend/src/main/java/com/werkspot/BackendApplication.java
+++ backend/src/main/java/com/werkspot/BackendApplication.java
@@ -1,13 +1,23 @@
 package com.werkspot;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @SpringBootApplication
+@RestControllerAdvice
+@OpenAPIDefinition
 public class BackendApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BackendApplication.class, args);
     }
 
+    @Bean
+    public ModelMapper getModelMapper(){
+        return new ModelMapper();
+    }
 }
