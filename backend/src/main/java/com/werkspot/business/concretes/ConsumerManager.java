d9d4dcf24145203a960dd94d03c3afc617070416
Furkan Karakus <furkankarakus01@hotmail.com>
2023-08-31 21:57:04 +0300

    fill-up in class

--- backend/src/main/java/com/werkspot/business/concretes/ConsumerManager.java
+++ backend/src/main/java/com/werkspot/business/concretes/ConsumerManager.java
@@ -1,4 +1,36 @@
 package com.werkspot.business.concretes;
 
-public class ConsumerManager {
+import com.werkspot.business.abstracts.ConsumerService;
+import com.werkspot.business.requests.CreateConsumerRequest;
+import com.werkspot.business.requests.UpdateConsumerRequest;
+import com.werkspot.business.responses.GetAllByIdConsumersResponse;
+import com.werkspot.business.responses.GetAllConsumersResponse;
+
+import java.util.List;
+
+public class ConsumerManager implements ConsumerService {
+    @Override
+    public List<GetAllConsumersResponse> getAllConsumers() {
+        return null;
+    }
+
+    @Override
+    public GetAllByIdConsumersResponse getById(int id) {
+        return null;
+    }
+
+    @Override
+    public void add(CreateConsumerRequest createConsumerRequest) {
+
+    }
+
+    @Override
+    public void update(UpdateConsumerRequest updateConsumerRequest) {
+
+    }
+
+    @Override
+    public void delete(int id) {
+
+    }
 }
