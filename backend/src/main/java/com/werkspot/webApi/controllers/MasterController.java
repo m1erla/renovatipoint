2634c4a72a848310bccd175570e0345f738e67ef
Furkan Karakus <furkankarakus01@hotmail.com>
2023-08-31 21:56:12 +0300

    Created controller webApi for consumers and masters

---
+++ backend/src/main/java/com/werkspot/webApi/controllers/MasterController.java
@@ -0,0 +1,47 @@
+package com.werkspot.webApi.controllers;
+
+import com.werkspot.business.abstracts.MasterService;
+import com.werkspot.business.requests.CreateMasterRequest;
+import com.werkspot.business.requests.UpdateMasterRequest;
+import com.werkspot.business.responses.GetAllByIdConsumersResponse;
+import com.werkspot.business.responses.GetAllByIdMastersResponse;
+import com.werkspot.business.responses.GetAllMastersResponse;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/masters")
+@AllArgsConstructor
+public class MasterController {
+
+    private MasterService masterService;
+
+    @GetMapping
+    public List<GetAllMastersResponse> getAllMasters() {
+        return masterService.getAll();
+    }
+
+    @GetMapping()
+    public GetAllByIdMastersResponse getAllById(@PathVariable int id) {
+        return masterService.getById(id);
+    }
+
+    @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public void add(@RequestBody CreateMasterRequest createMasterRequest){
+           this.masterService.add(createMasterRequest);
+    }
+
+    @PutMapping
+    public void update(@RequestBody UpdateMasterRequest masterRequest){
+        this.masterService.update(masterRequest);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable int id){
+        this.delete(id);
+    }
+}
