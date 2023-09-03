67ef3f5d683cc7af534e6c5221658cf007ce7590
Furkan Karakus <furkankarakus01@hotmail.com>
2023-09-03 21:39:14 +0300

    Jwt auth tokens just set-up for impl

--- backend/src/main/java/com/werkspot/BackendApplication.java
+++ backend/src/main/java/com/werkspot/BackendApplication.java
@@ -14,6 +14,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -48,6 +49,10 @@ public class BackendApplication {
         }
 
         return validationProblemDetails;
+    }
+    @GetMapping("/")
+    public String apiRoot(){
+        return "Hello";
     }
     @Bean
     public ModelMapper getModelMapper(){
