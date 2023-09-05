bdd374ce66938b89df53b6e674f02cceda49149f
Furkan Karakus <furkankarakus01@hotmail.com>
2023-09-05 15:10:05 +0300

    Jwt auth package is finish.

--- backend/src/main/java/com/werkspot/security/auth/RegisterRequest.java
+++ backend/src/main/java/com/werkspot/security/auth/RegisterRequest.java
@@ -1,4 +1,20 @@
 package com.werkspot.security.auth;
 
+import com.werkspot.security.consumer.Role;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class RegisterRequest {
+    private String name;
+    private String surname;
+    private String email;
+    private String password;
+    private String phone_number;
+    private Role role;
 }
