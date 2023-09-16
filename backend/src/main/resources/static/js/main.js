e63dc03916522b9c199c21fdf0110141466498f2
Furkan Karakus <furkankarakus01@hotmail.com>
2023-09-16 15:25:30 +0300

    Websocket processing...

---
+++ backend/src/main/resources/static/js/main.js
@@ -0,0 +1,6 @@
+'use strict';
+
+var usernamePage = document.querySelector('#username-page');
+var chatPage = document.querySelector('#chat-page');
+var usernameForm = document.querySelector('#usernameForm');
+var messageForm = document.querySelector('#messageForm');
