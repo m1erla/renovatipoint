43ce8564a4f083e7e65da84d52c3d62405c93f75
m1erla <116915007+m1erla@users.noreply.github.com>
2023-11-16 20:22:33 +0300

    feat: UserController revised.

--- backend/src/main/java/com/werkspot/webApi/controllers/UserController.java
+++ backend/src/main/java/com/werkspot/webApi/controllers/UserController.java
@@ -20,7 +20,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/users")
 @AllArgsConstructor
-@CrossOrigin(origins = "https://myklus.onrender.com", allowCredentials = "*", allowedHeaders = "*")
+@CrossOrigin(origins = "https://myklus.onrender.com", allowCredentials = "true", allowedHeaders = "*")
 public class UserController {
     private final UserService userService;
     private final JwtService jwtService;
