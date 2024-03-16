761ae75a2bf58fb1572c021ee1001aa5c9c9b61a
m1erla <116915007+m1erla@users.noreply.github.com>
2024-03-16 18:51:23 +0300

    revised DemoController

--- backend/src/main/java/com/werkspot/security/demo/DemoController.java
+++ backend/src/main/java/com/werkspot/security/demo/DemoController.java
@@ -1,8 +1,7 @@
 package com.werkspot.security.demo;
 
-import com.werkspot.business.responses.GetUserByTokenResponse;
+
 import io.swagger.v3.oas.annotations.Hidden;
-import org.apache.coyote.Response;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
