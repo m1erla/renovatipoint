92e33e97c6da43729da32a5bd57a380fe1f70977
m1erla <furkankarakus01@hotmail.com>
2025-11-08 22:06:13 +0300

    revert baseURL to local development environment

--- src/utils/api.jsx
+++ src/utils/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || "https://buildbetter.onrender.com",
+  baseURL: process.env.REACT_APP_API_URL || "http://localhost:8080",
   headers: {
     "Content-Type": "application/json",
   },
